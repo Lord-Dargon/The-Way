7fca17f24218ac3c6fb474a525f6fcf010d4694c
Lord-Dargon <kidspidy07@gmail.com>
2024-01-21 00:03:15 -0500

    Added button operation
    Check the test scene by clicking on the character and then the buttons while looking at the debug log

---
+++ Scripts/Test_Player.gd
@@ -0,0 +1,53 @@
+extends Node
+
+#Stats
+@export var HP: int = 100
+@export var Def: int = 100
+@export var Atk: int = 100
+@export var Spd: int = 100
+
+@onready var character_button = $Character_Button
+@onready var button = $Character_Button/Menu/Panel/Button
+@onready var menu = $Character_Button/Menu/Panel
+@onready var Action_bar = $Timer
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Action_bar.set_wait_time(100/Spd)
+	Action_bar.start()
+	pass # Replace with function body.
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	print(Action_bar.time_left)
+	pass
+
+
+
+func _on_attack_1_pressed():
+	if Action_bar.is_stopped():
+		print("Attack_1")
+		Action_bar.start()
+		pass #
+
+func _on_attack_2_pressed():
+	if Action_bar.is_stopped():
+		print("Attack_2")
+		Action_bar.start()
+	pass
+	
+func _on_attack_3_pressed():
+	if Action_bar.is_stopped():
+		print("Attack_3")
+		Action_bar.start()
+		pass # Replace with function body.
+
+
+
+func _on_character_button_toggled(toggled_on):
+	menu.show()
+	
+	if toggled_on == false:
+		menu.hide()
